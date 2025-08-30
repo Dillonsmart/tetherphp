04d4305a1eb282714c0674f8b04913ab99cf2113
Dillon Smart <dillonsmart00@gmail.com>
2025-08-30 14:15:11 +0100

    removed imported font

--- app/Views/partials/header.php
+++ app/Views/partials/header.php
@@ -3,6 +3,13 @@
 <head>
     <title><?php echo $pageTitle ?? env('APP_NAME') ;?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=TASA+Explorer:wght@400..800&display=swap" rel="stylesheet">
+
+    <!-- styles -->
     <link rel="stylesheet" href="/css/reset.css">
     <link rel="stylesheet" href="/css/app.css">
 </head>
