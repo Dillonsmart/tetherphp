e8cc4f43d4db41ba79d07e55f968ffd65a6d488e
Dillon Smart <dillonsmart00@gmail.com>
2025-09-03 19:24:06 +0100

    Updated env example and using env var in view

--- app/Views/pages/home/index.php
+++ app/Views/pages/home/index.php
@@ -1,8 +1,8 @@
 <?php
 view('partials/header'); ?>
     <section class="text-center my-48 px-6">
-        <h1 class="text-6xl lg:text-8xl mb-6">TetherPHP</h1>
-        <h2 class="text-3xl lg:text-4xl mb-4">A Refined ADR Framework for PHP</h2>
-        <p style="text-align: center; margin-bottom: 0; font-size: 0.8rem;"><a href="https://github.com/DillonSmart/tetherphp" target="_blank">GitHub</a> - <a href="https://packagist.org/packages/dillonsmart/tetherphp">Packagist</a> - <a href="https://x.com/dillon_smart">Follow the development</a></p>
+        <h1 class="text-6xl lg:text-8xl mb-6"><?php echo env('APP_NAME'); ?></h1>
+        <h2 class="text-3xl lg:text-4xl mb-4">Ship PHP faster, with clarity</h2>
+        <p class="text-xs"><a href="https://github.com/DillonSmart/tetherphp" target="_blank">GitHub</a> - <a href="https://packagist.org/packages/dillonsmart/tetherphp">Packagist</a> - <a href="https://x.com/dillon_smart">Follow the development</a></p>
     </section>
 <?php view('partials/footer'); ?>
